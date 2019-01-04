ef14269a17fd1bb3de272b0c317836a63ede7b8d
alexSmkh <alexshikari9333@gmail.com>
2019-01-04 22:43:07 +0300

    Added mixin for post and tag detail

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Post, Tag
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
 
-
+from .models import Post, Tag
+from .utils import ObjectDetailMixin
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
@@ -15,13 +15,11 @@ def tags_list(request):
     return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
-class PostDetail(View):
-    def get(self, request, slug):
-        post = get_object_or_404(Post, slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 
-class TagDetail(View):
-    def get(self, request, slug):
-        tag = get_object_or_404(Tag, slug__iexact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin,View):
+    model = Tag
+    template = 'blog/tag_detail.html'
