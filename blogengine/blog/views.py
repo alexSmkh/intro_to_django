85ff0519ef801da68797b4e0f61587335e8630b0
alexSmkh <alexshikari9333@gmail.com>
2019-01-04 22:23:03 +0300

    Replaced method for tag detail with class

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from .models import Post, Tag
 from django.views.generic import View
 
+
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-# def post_detail(request, slug):
-#     post = Post.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/post_detail.html', context={'post':post})
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
 class PostDetail(View):
@@ -19,11 +20,7 @@ class PostDetail(View):
         return render(request, 'blog/post_detail.html', context={'post': post})
 
 
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
-
-
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
