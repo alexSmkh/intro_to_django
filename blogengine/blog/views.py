3dfc9def30e4f95992ab40583d91aca82ad8745d
alexSmkh <alexshikari9333@gmail.com>
2019-01-23 20:20:47 +0300

    Added form for creating tags

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views.generic import View
 from django.shortcuts import get_object_or_404
+from .forms import TagForm
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
+
+
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
@@ -20,6 +24,23 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class TagDetail(ObjectDetailMixin,View):
+class TagCreate(View):
+    def get(self, request):
+        form = TagForm()
+        return render(request, 'blog/tag_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = TagForm(request.POST)
+
+        if bound_form.is_valid():
+            new_tag = bound_form.save()
+            return redirect(new_tag)
+        return render(
+            request,
+            'blog/tag_create.html',
+            context={'form': bound_form})
+
+
+class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
