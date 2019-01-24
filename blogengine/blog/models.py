279cb35e05a4d2404637a2fe0c294c81bbb616ce
alexSmkh <alexshikari9333@gmail.com>
2019-01-24 14:30:18 +0300

    Added the creation of a unique slug for the new post

--- blogengine/blog/models.py
+++ blogengine/blog/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
 
 
-# Create your models here.
+def gen_slug(post_title):
+    first_part_slug = slugify(post_title, allow_unicode=True)
+    return '{new_slug}-{time_stamp}'.format(
+        new_slug=first_part_slug,
+        time_stamp=str(int(time())),
+    )
+
+
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     date_pub = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return '{}'.format(self.title)
